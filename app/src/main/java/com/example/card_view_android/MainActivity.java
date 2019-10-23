995d031e75a7d7295f15545a95a798e3e4c3ad9b
Leonardo Melo <leonardo.jesus@hdi.com.br>
2019-10-23 14:15:45 -0300

    Card View finalizado

--- app/src/main/java/com/example/card_view_android/MainActivity.java
+++ app/src/main/java/com/example/card_view_android/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.card_view_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt1, bt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt1 = findViewById(R.id.bt1);
+        bt2 = findViewById(R.id.bt2);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "VAI CCORINTHIANS!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "THE BEST OF THE WORLD!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
